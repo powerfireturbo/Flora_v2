917f168687a3642dcb2575653e6fe044528a5166
Chris Moore <rochrismo@gmail.com>
2019-04-26 17:06:43 -0400

    add ResultsPageViewModel and FamilyList container

---
+++ Flora/Flora/Models/FamilyList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace Flora.Models
+{
+    public class FamilyList: List<Plant>
+    {
+        public string FamilyName { get; set; }
+        public List<Plant> Plants => this;
+
+        public FamilyList(string family, List<Plant> plants)
+        {
+            FamilyName = family;
+            AddRange(plants);
+        }
+    }
+}
---
+++ Flora/Flora/ViewModels/Location/ResultsPageViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Flora.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Flora.ViewModels.Location
+{
+    class ResultsPageViewModel : INotifyPropertyChanged
+    {
+        public List<FamilyList> ResultsList { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ResultsPageViewModel(List<Plant> plants)
+        {
+
+        }
+
+        private void AddList(List<Plant> plants)
+        {
+            List<string> families = new List<string>();
+            foreach(Plant p in plants)
+            {
+                if (!families.Contains(p.Family))
+                {
+                    families.Add(p.Family);
+                }
+            }
+            foreach(string f in families)
+            {
+                FamilyList famList = new FamilyList(f, plants.FindAll(p => p.Family == f));
+            }
+            OnPropertyChanged("ResultsList");
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
